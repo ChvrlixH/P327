3d7ff90ae83578e3b936e6c3618859afd0dcbd27
Huseyn Sh. Ahadzada <huseynsha@code.local>
2022-11-03 19:51:02 +0400

    Matrix geri qaytarildi

--- matrix.bat
+++
@@ -1,5 +0,0 @@
-echo off 
-	color c 
-	:start 
-	echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%System.32%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-	goto start
