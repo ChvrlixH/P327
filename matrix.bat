faa9dad5e0967bf007e7f6e1c724804a60c0fade
Huseyn Sh. Ahadzada <huseynsha@code.local>
2022-11-03 19:25:16 +0400

    Matrix effekt

---
+++ matrix.bat
@@ -0,0 +1,5 @@
+echo off 
+	color c 
+	:start 
+	echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%System.32%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+	goto start
